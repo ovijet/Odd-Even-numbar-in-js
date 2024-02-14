f175b0ac623d383ffbe37f2c07723279b8bf32c8
ovijet <ovijethalder143@gmail.com>
2024-02-14 17:22:55 +0600

    odd and even numbar

---
+++ loop.js
@@ -0,0 +1,18 @@
+
+//even numbar 
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+
+//odd numbar
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 1) {
+      console.log(i);
+    }
+  }
+  
